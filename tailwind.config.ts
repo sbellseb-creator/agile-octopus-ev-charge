638304d69769cd218053776ab0b3a18f6372f775
sbellseb-creator <sbellseb@gmail.com>
2026-09-04 14:04:22 +0100

    Add grid-rows-2 to Tailwind safelist to ensure CSS is generated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,10 @@ export default {
   darkMode: ["class"],
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   prefix: "",
+  safelist: [
+    "grid-rows-2",
+    "grid-cols-4",
+  ],
   theme: {
     container: {
       center: true,
